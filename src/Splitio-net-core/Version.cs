d1a19a0a43f79cce208ceeb9f38d3c5e30628b56
julianwaimann <jwaimann@makingsense.com>
2017-03-28 13:19:32 -0300

    Update version for release

--- src/Splitio-net-core/Version.cs
+++ src/Splitio-net-core/Version.cs
@@ -2,7 +2,7 @@
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "1.0.0-rc10";
+        public static string SplitSdkVersion = "1.0.0";
         public static string SplitSpecVersion = "1.0";
     }
 }
