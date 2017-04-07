a0da64455590a005655caa650f3b0632eaae9e9c
julianwaimann <jwaimann@makingsense.com>
2017-04-07 17:22:54 -0300

    Update csproj to include murmur in nuget dependencies

--- src/Splitio-net-core/Version.cs
+++ src/Splitio-net-core/Version.cs
@@ -2,7 +2,7 @@
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "2.0.0-rc23";
+        public static string SplitSdkVersion = "2.0.0-rc34";
         public static string SplitSpecVersion = "1.0";
     }
 }
