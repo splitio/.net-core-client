293249aa7491d9ac765e881f4d1f05004515eb1c
julianwaimann <jwaimann@makingsense.com>
2017-04-24 17:27:32 -0300

    Implement test to verify that boolean attribute returns false

--- src/Splitio-net-core/Version.cs
+++ src/Splitio-net-core/Version.cs
@@ -2,7 +2,7 @@
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "3.0.0";
+        public static string SplitSdkVersion = "3.0.1-rc1";
         public static string SplitSpecVersion = "1.0";
     }
 }
