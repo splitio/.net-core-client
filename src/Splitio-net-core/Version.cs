5ca84ded4dda458cd987c566a728cf926ca9f892
julianwaimann <jwaimann@makingsense.com>
2017-06-26 12:17:09 -0300

    Update version for prerelease

--- src/Splitio-net-core/Version.cs
+++ src/Splitio-net-core/Version.cs
@@ -2,7 +2,7 @@
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "3.2.0-rc123";
+        public static string SplitSdkVersion = "3.2.0-rc161";
         public static string SplitSpecVersion = "1.0";
     }
 }
