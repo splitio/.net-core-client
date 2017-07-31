6ad827e83885489efab7db5daca75371a2b2ffdc
julianwaimann <jwaimann@makingsense.com>
2017-07-31 16:03:14 -0300

    Implement fix

--- Splitio-net-core-tests/Unit Tests/Client/SplitManagerUnitTests.cs
+++ Splitio-net-core-tests/Unit Tests/Client/SplitManagerUnitTests.cs
@@ -17,12 +17,22 @@ namespace Splitio_Tests.Unit_Tests.Client
             var conditionsWithLogic = new List<ConditionWithLogic>();
             var conditionWithLogic = new ConditionWithLogic()
             {
+                conditionType = ConditionType.WHITELIST,
+                partitions = new List<PartitionDefinition>()
+                {
+                    new PartitionDefinition(){size = 100, treatment = "off"}
+                }
+            };
+            conditionsWithLogic.Add(conditionWithLogic);
+            var conditionWithLogic2 = new ConditionWithLogic()
+            {
+                conditionType = ConditionType.ROLLOUT,
                 partitions  = new List<PartitionDefinition>()
                 {
                     new PartitionDefinition(){size = 100, treatment = "on"}
                 }
             };
-            conditionsWithLogic.Add(conditionWithLogic);
+            conditionsWithLogic.Add(conditionWithLogic2);
             var splitCache = new InMemorySplitCache(new ConcurrentDictionary<string, ParsedSplit>());
             splitCache.AddSplit("test1", new ParsedSplit() { name = "test1", changeNumber = 10000, killed = false, trafficTypeName = "user", seed = -1, conditions = conditionsWithLogic});
             splitCache.AddSplit("test2", new ParsedSplit() { name = "test2", conditions = conditionsWithLogic });
@@ -50,12 +60,52 @@ namespace Splitio_Tests.Unit_Tests.Client
         }
 
         [TestMethod]
+        public void SplitsReturnWithNoRolloutConditionSuccessfully()
+        {
+            //Arrange
+            var conditionsWithLogic = new List<ConditionWithLogic>();
+            var conditionWithLogic = new ConditionWithLogic()
+            {
+                conditionType = ConditionType.WHITELIST,
+                partitions = new List<PartitionDefinition>()
+                {
+                    new PartitionDefinition(){size = 100, treatment = "on"}
+                }
+            };
+            conditionsWithLogic.Add(conditionWithLogic);
+            var splitCache = new InMemorySplitCache(new ConcurrentDictionary<string, ParsedSplit>());
+            splitCache.AddSplit("test1", new ParsedSplit() { name = "test1", changeNumber = 10000, killed = false, trafficTypeName = "user", seed = -1, conditions = conditionsWithLogic });
+            splitCache.AddSplit("test2", new ParsedSplit() { name = "test2", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test3", new ParsedSplit() { name = "test3", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test4", new ParsedSplit() { name = "test4", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test5", new ParsedSplit() { name = "test5", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test6", new ParsedSplit() { name = "test6", conditions = conditionsWithLogic });
+
+            var manager = new SplitManager(splitCache);
+
+            //Act
+            var result = manager.Splits();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(6, result.Count);
+            var firstResult = result.Find(x => x.name == "test1");
+            Assert.AreEqual(firstResult.name, "test1");
+            Assert.AreEqual(firstResult.changeNumber, 10000);
+            Assert.AreEqual(firstResult.killed, false);
+            Assert.AreEqual(firstResult.trafficType, "user");
+            Assert.AreEqual(firstResult.treatments.Count, 0);
+        }
+
+
+        [TestMethod]
         public void SplitReturnSuccessfully()
         {
             //Arrange
             var conditionsWithLogic = new List<ConditionWithLogic>();
             var conditionWithLogic = new ConditionWithLogic()
             {
+                conditionType = ConditionType.ROLLOUT,
                 partitions = new List<PartitionDefinition>()
                 {
                     new PartitionDefinition(){size = 100, treatment = "on"}
@@ -85,6 +135,90 @@ namespace Splitio_Tests.Unit_Tests.Client
             var firstTreatment = result.treatments[0];
             Assert.AreEqual(firstTreatment, "on");
         }
+
+        [TestMethod]
+        public void SplitReturnRolloutConditionTreatmentsSuccessfully()
+        {
+            //Arrange
+            var conditionsWithLogic = new List<ConditionWithLogic>();
+            var conditionWithLogic = new ConditionWithLogic()
+            {
+                conditionType = ConditionType.WHITELIST,
+                partitions = new List<PartitionDefinition>()
+                {
+                    new PartitionDefinition(){size = 100, treatment = "on"},
+                }
+            };
+            conditionsWithLogic.Add(conditionWithLogic);
+
+            var conditionWithLogic2 = new ConditionWithLogic()
+            {
+                conditionType = ConditionType.ROLLOUT,
+                partitions = new List<PartitionDefinition>()
+                {
+                    new PartitionDefinition(){size = 90, treatment = "on"},
+                    new PartitionDefinition(){size = 10, treatment = "off"},
+                }
+            };
+            conditionsWithLogic.Add(conditionWithLogic2);
+
+            var splitCache = new InMemorySplitCache(new ConcurrentDictionary<string, ParsedSplit>());
+            splitCache.AddSplit("test1", new ParsedSplit() { name = "test1", changeNumber = 10000, killed = false, trafficTypeName = "user", seed = -1, conditions = conditionsWithLogic });
+            splitCache.AddSplit("test2", new ParsedSplit() { name = "test2", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test3", new ParsedSplit() { name = "test3", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test4", new ParsedSplit() { name = "test4", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test5", new ParsedSplit() { name = "test5", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test6", new ParsedSplit() { name = "test6", conditions = conditionsWithLogic });
+
+            var manager = new SplitManager(splitCache);
+
+            //Act
+            var result = manager.Split("test1");
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.name, "test1");
+            Assert.AreEqual(result.treatments.Count, 2);
+            var firstTreatment = result.treatments[0];
+            Assert.AreEqual(firstTreatment, "on");
+            var secondTreatment = result.treatments[1];
+            Assert.AreEqual(secondTreatment, "off");
+        }
+
+        [TestMethod]
+        public void SplitReturnEmptyTreatmentsWhenNoRolloutCondition()
+        {
+            //Arrange
+            var conditionsWithLogic = new List<ConditionWithLogic>();
+            var conditionWithLogic = new ConditionWithLogic()
+            {
+                conditionType = ConditionType.WHITELIST,
+                partitions = new List<PartitionDefinition>()
+                {
+                    new PartitionDefinition(){size = 100, treatment = "on"},
+                }
+            };
+            conditionsWithLogic.Add(conditionWithLogic);
+
+            var splitCache = new InMemorySplitCache(new ConcurrentDictionary<string, ParsedSplit>());
+            splitCache.AddSplit("test1", new ParsedSplit() { name = "test1", changeNumber = 10000, killed = false, trafficTypeName = "user", seed = -1, conditions = conditionsWithLogic });
+            splitCache.AddSplit("test2", new ParsedSplit() { name = "test2", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test3", new ParsedSplit() { name = "test3", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test4", new ParsedSplit() { name = "test4", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test5", new ParsedSplit() { name = "test5", conditions = conditionsWithLogic });
+            splitCache.AddSplit("test6", new ParsedSplit() { name = "test6", conditions = conditionsWithLogic });
+
+            var manager = new SplitManager(splitCache);
+
+            //Act
+            var result = manager.Split("test1");
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.name, "test1");
+            Assert.AreEqual(result.treatments.Count, 0);
+        }
+
 
         [TestMethod]
         public void SplitReturnsNullWhenInexistent()
