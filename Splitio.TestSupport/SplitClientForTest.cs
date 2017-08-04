80401bbd96ebb2c55c4f85cd6cea7cc7aa51bab4
julianwaimann <jwaimann@makingsense.com>
2017-08-04 17:46:05 -0300

    Implement test support on net core

---
+++ Splitio.TestSupport/SplitClientForTest.cs
@@ -0,0 +1,58 @@
+﻿using Splitio.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Splitio.Services.Client.Classes
+{
+    public class SplitClientForTest : SplitClient
+    {
+        private Dictionary<string, string> _tests;
+
+        public SplitClientForTest()
+        {
+            _tests = new Dictionary<string, string>();
+        }
+
+        public override void Destroy()
+        {
+            _tests = new Dictionary<string, string>();
+        }
+
+        public void ClearTreatments()
+        {
+            _tests = new Dictionary<string, string>();
+        }
+
+        public void RegisterTreatments(Dictionary<string, string> treatments)
+        {
+            foreach (var treatment in treatments)
+            {
+                if (!_tests.ContainsKey(treatment.Key))
+                {
+                    _tests.Add(treatment.Key, treatment.Value);
+                }
+            }
+        }
+
+        public void RegisterTreatment(string feature, string treatment)
+        {
+            _tests.Add(feature, treatment);
+        }
+
+        public string GetTreatment(string key, string feature)
+        {
+            return _tests.ContainsKey(feature) ? _tests[feature] : "control";
+        }
+
+        public string GetTreatment(string key, string feature, Dictionary<string, object> attributes)
+        {
+            return this.GetTreatment(key, feature);
+        }
+
+        public string GetTreatment(Key key, string feature, Dictionary<string, object> attributes)
+        {
+            return _tests.ContainsKey(feature) ? _tests[feature] : "control";
+        }
+    }
+}
+
---
+++ Splitio.TestSupport/SplitTest.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace Splitio.TestSupport
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class SplitTest : ClassDataAttribute
+    {
+        public string feature { get; private set; }
+        public string[] treatments { get; private set; }
+
+        [JsonConstructor]
+        public SplitTest(string feature, string[] treatments)
+            : base(typeof(object))
+        {
+            this.feature = feature;
+            this.treatments = treatments;
+        }
+
+        public SplitTest(string test) : base(typeof(object))
+        {
+            var splitTest = JsonConvert.DeserializeObject<SplitTest>(test);
+            this.feature = splitTest.feature;
+            this.treatments = splitTest.treatments;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest)
+        {
+            var data = new List<string[]>();
+            foreach (var treatment in treatments)
+            {
+                data.Add(new string[] { feature, treatment });
+            }
+            return data;
+        }
+    }
+}
