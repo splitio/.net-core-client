2adae6b35dfbd12e6d24e1813353e66ab5a8d307
TinchoIgnacio <34524510+TinchoIgnacio@users.noreply.github.com>
2018-01-04 23:48:51 -0300

    Use dynamic version number (#38)

--- src/Splitio-net-core/Version.cs
+++ src/Splitio-net-core/Version.cs
@@ -1,8 +1,10 @@
-﻿namespace Splitio
+﻿using System.Reflection;
+
+namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "3.3.2";
+        public static string SplitSdkVersion = typeof(Version).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
         public static string SplitSpecVersion = "1.0";
     }
 }
