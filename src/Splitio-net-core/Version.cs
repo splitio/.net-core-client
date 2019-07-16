a21517d2e59d2ed223c1a03440cb66dfbbce403f
sanzmauro <mauro.sanz@split.io>
2019-07-16 15:20:48 -0300

    wip

--- src/Splitio-net-core/Version.cs
+++ src/Splitio-net-core/Version.cs
@@ -5,6 +5,7 @@ namespace Splitio
     public static class Version
     {
         public static string SplitSdkVersion = typeof(Version).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
         public static string SplitSpecVersion = "1.0";
     }
 }
