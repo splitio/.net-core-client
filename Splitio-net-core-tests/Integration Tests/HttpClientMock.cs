f02ebe57500e5100b2410ec9ab7f4e0cbd020224
sanzmauro <mauro.sanz@split.io>
2019-08-27 13:19:16 -0300

    wip

---
+++ Splitio-net-core-tests/Integration Tests/HttpClientMock.cs
@@ -0,0 +1,41 @@
+﻿#if !NET45
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace Splitio_Tests.Integration_Tests
+{
+    public class HttpClientMock
+    {
+        private readonly FluentMockServer _server;
+
+        public HttpClientMock()
+        {
+            _server = FluentMockServer.Start(50286);
+        }
+
+        public void SplitChangesEndpointMock(int statusCodeExpected)
+        {
+            var body = statusCodeExpected == 200
+                ? @"{ msg: ""Hello world!""}"
+                : "";
+
+            _server
+                .Given(
+                    Request.Create()
+                    .WithPath("/api/splitChanges?since=-1")
+                    .UsingGet()
+                )
+                .RespondWith(
+                    Response.Create()
+                    .WithStatusCode(statusCodeExpected)
+                    .WithBody(body));
+        }
+
+        public void ShutdownServer()
+        {
+            _server.Stop();
+        }
+    }
+}
+#endif
---
+++ Splitio-net-core-tests/Integration Tests/IntegrationsTests.cs
@@ -0,0 +1,39 @@
+﻿#if !NET45
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Splitio.Services.Client.Classes;
+
+namespace Splitio_Tests.Integration_Tests
+{
+    [TestClass]
+    public class IntegrationsTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var httpClientMock = new HttpClientMock();
+
+            httpClientMock.SplitChangesEndpointMock(200);
+
+            var apikey = "chirimbolito";
+
+            var configurations = new ConfigurationOptions
+            {
+                FeaturesRefreshRate = 30,
+                SegmentsRefreshRate = 30,
+                Endpoint = "http://localhost:50286",
+                EventsEndpoint = "http://localhost:50286",
+                ReadTimeout = 20000,
+                ConnectionTimeout = 20000,
+                ImpressionsRefreshRate = 15
+            };
+
+            var factory = new SplitFactory(apikey, configurations);
+
+            var client = factory.Client();
+            client.BlockUntilReady(100000);
+
+            httpClientMock.ShutdownServer();
+        }
+    }
+}
+#endif
