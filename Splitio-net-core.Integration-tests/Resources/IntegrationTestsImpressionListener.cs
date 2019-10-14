473b6d5a52c8fe06a7eb6a7498be716a045b138c
Mauro Sanz <51236193+sanzmauro@users.noreply.github.com>
2019-10-14 12:45:20 -0300

    Add basic TC's to integration tests suites (#94)

---
+++ Splitio-net-core.Integration-tests/Resources/IntegrationTestsImpressionListener.cs
@@ -0,0 +1,30 @@
+﻿using Splitio.Domain;
+using Splitio.Services.Impressions.Interfaces;
+using Splitio.Services.Shared.Classes;
+
+namespace Splitio_net_core.Integration_tests.Resources
+{
+    public class IntegrationTestsImpressionListener : IImpressionListener
+    {
+        BlockingQueue<KeyImpression> queue;
+
+        public IntegrationTestsImpressionListener(int size)
+        {
+            queue = new BlockingQueue<KeyImpression>(size);
+        }
+
+        public void Log(KeyImpression impression)
+        {
+            if (queue.HasReachedMaxSize())
+            {
+                queue.Dequeue();
+            }
+            queue.Enqueue(impression);
+        }
+
+        public BlockingQueue<KeyImpression> GetQueue()
+        {
+            return queue;
+        }
+    }
+}
---
+++ Splitio-net-core.Integration-tests/Resources/KeyImpressionBackend.cs
@@ -0,0 +1,35 @@
+﻿using Splitio.Domain;
+using System.Collections.Generic;
+
+namespace Splitio_net_core.Integration_tests.Resources
+{
+    public class KeyImpressionBackend
+    {
+        public string TestName { get; set; }
+        public List<KeyImpression> KeyImpressions { get; set; }
+    }
+
+    public class KeyImpressionRedis
+    {
+        public MachineRedis M { get; set; }
+        public InfoRedis I { get; set; }
+    }
+
+    public class MachineRedis
+    {
+        public string S { get; set; }
+        public string I { get; set; }
+        public string N { get; set; }
+    }
+
+    public class InfoRedis
+    {
+        public string K { get; set; }
+        public string B { get; set; }
+        public string F { get; set; }
+        public string T { get; set; }
+        public string R { get; set; }
+        public long? C { get; set; }
+        public long? M { get; set; }
+    }
+}
