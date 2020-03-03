29918b242e0baa14a00265478b674326e450664f
Cristian Daniel Fadón <cristian.fadon@split.io>
2020-03-03 15:14:02 -0300

    Removed commas

--- sonar-scanner.bat
+++ sonar-scanner.bat
@@ -20,10 +20,10 @@ EXIT /B 0
 IF NOT "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
   echo Pull Request number %APPVEYOR_PULL_REQUEST_NUMBER%
   CALL :sonar_scanner ^
-    /d:sonar.pullrequest.provider="GitHub", ^
-    /d:sonar.pullrequest.github.repository="splitio/.net-core-client", ^
-    /d:sonar.pullrequest.key=%APPVEYOR_PULL_REQUEST_NUMBER%, ^
-    /d:sonar.pullrequest.branch=%APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH%, ^
+    /d:sonar.pullrequest.provider="GitHub" ^
+    /d:sonar.pullrequest.github.repository="splitio/.net-core-client" ^
+    /d:sonar.pullrequest.key=%APPVEYOR_PULL_REQUEST_NUMBER% ^
+    /d:sonar.pullrequest.branch=%APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH% ^
     /d:sonar.pullrequest.base=%APPVEYOR_REPO_BRANCH%
 ) ELSE (
     IF "%APPVEYOR_REPO_BRANCH%"=="master" (
@@ -42,7 +42,7 @@ IF NOT "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
         echo Branch Name is %APPVEYOR_REPO_BRANCH%
         echo Target Branch is !TARGET_BRANCH!
         CALL :sonar_scanner ^
-          "/d:sonar.branch.name="%APPVEYOR_REPO_BRANCH%"" , ^
+          "/d:sonar.branch.name="%APPVEYOR_REPO_BRANCH%"" ^
           "/d:sonar.branch.target="!TARGET_BRANCH!""
         )
   )
