b30275d87d0fe474b475954097ba9e24c4e8861b
Cristian Daniel Fadón <cristian.fadon@split.io>
2020-03-03 14:09:27 -0300

    Functions order

--- sonar-scanner.bat
+++ sonar-scanner.bat
@@ -1,6 +1,22 @@
 @echo off
 setlocal EnableDelayedExpansion
+GOTO :main
 
+:sonar_scanner
+echo First parameter is %~1
+echo Second parameter is %~2
+SonarScanner.MSBuild.exe begin ^
+  /n:".net-core-client" ^
+  /k:"net-core-client" ^
+  /d:sonar.host.url="https://sonarqube.split-internal.com" ^
+  /d:sonar.login=%SONAR_LOGIN% ^
+  /d:sonar.ws.timeout="300" ^
+  /d:sonar.links.ci="https://travis-ci.com/splitio/.net-core-client" ^
+  /d:sonar.links.scm="https://github.com/splitio/.net-core-client" ^
+  %*
+EXIT /B 0
+
+:main
 IF NOT "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
   echo Pull Request number %APPVEYOR_PULL_REQUEST_NUMBER%
   CALL :sonar_scanner ^
@@ -30,19 +46,3 @@ IF NOT "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
           "/d:sonar.branch.target=!TARGET_BRANCH!"
         )
   )
-
-:: Functions
-
-:sonar_scanner
-echo First parameter is %~1
-echo Second parameter is %~2
-SonarScanner.MSBuild.exe begin ^
-  /n:".net-core-client" ^
-  /k:"net-core-client" ^
-  /d:sonar.host.url="https://sonarqube.split-internal.com" ^
-  /d:sonar.login=%SONAR_LOGIN% ^
-  /d:sonar.ws.timeout="300" ^
-  /d:sonar.links.ci="https://travis-ci.com/splitio/.net-core-client" ^
-  /d:sonar.links.scm="https://github.com/splitio/.net-core-client" ^
-  %*
-EXIT /B 0
