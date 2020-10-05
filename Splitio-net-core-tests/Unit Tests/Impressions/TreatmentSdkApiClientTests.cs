7835da46cde850cfb089c48a7930b7e4480cc794
sanzmauro <mauro.sanz@split.io>
2020-10-05 10:42:01 -0300

    fix build

--- Splitio-net-core-tests/Unit Tests/Impressions/TreatmentSdkApiClientTests.cs
+++ Splitio-net-core-tests/Unit Tests/Impressions/TreatmentSdkApiClientTests.cs
@@ -52,26 +52,5 @@ namespace Splitio_Tests.Unit_Tests.Impressions
             var expected = "[{\"f\":\"feature-1\",\"i\":[{\"k\":\"matching-key\",\"t\":\"treatment\",\"m\":34534546,\"c\":3333444,\"r\":\"label\",\"b\":\"bucketing-key\"},{\"k\":\"matching-key\",\"t\":\"treatment\",\"m\":34534550,\"c\":3333444,\"r\":\"label\",\"b\":\"bucketing-key\"}]},{\"f\":\"feature-2\",\"i\":[{\"k\":\"matching-key\",\"t\":\"treatment\",\"m\":34534546,\"c\":3333444,\"r\":\"label\",\"b\":\"bucketing-key\"}]}]";
             Assert.AreEqual(expected, result);
         }
-
-        [TestMethod]
-        public void CorrectFormatSendImpressions()
-        {
-            // Arrange.
-            var impressions = new List<KeyImpression>
-            {
-                new KeyImpression("matching-key", "feature-1", "treatment", 34534546, 3333444, "label", "bucketing-key"),
-                new KeyImpression("matching-key", "feature-1", "treatment", 34534550, 3333444, "label", "bucketing-key", 34534546),
-                new KeyImpression("matching-key", "feature-2", "treatment", 34534546, 3333444, "label", "bucketing-key"),
-            };
-
-            var treatmentSdkApiClient = new TreatmentSdkApiClient(new HTTPHeader(), "http://www.fake-test-split.com", 5, 5);
-
-            // Act.
-            var result = treatmentSdkApiClient.ConvertToJson(impressions);
-
-            // Assert.
-            var expected = "[{\"f\":\"feature-1\",\"i\":[{\"k\":\"matching-key\",\"t\":\"treatment\",\"m\":34534546,\"c\":3333444,\"r\":\"label\",\"b\":\"bucketing-key\"},{\"k\":\"matching-key\",\"t\":\"treatment\",\"m\":34534550,\"c\":3333444,\"r\":\"label\",\"b\":\"bucketing-key\"}]},{\"f\":\"feature-2\",\"i\":[{\"k\":\"matching-key\",\"t\":\"treatment\",\"m\":34534546,\"c\":3333444,\"r\":\"label\",\"b\":\"bucketing-key\"}]}]";
-            Assert.AreEqual(expected, result);
-        }
     }
 }
